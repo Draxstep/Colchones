5b144c00682b3326fe04473455e05b94f29b1302
XxSuarezxX <141874132+XxSuarezxX@users.noreply.github.com>
2024-05-29 23:39:51 -0500

    Update ModelosColchonFacturaDAO.java

--- src/dao/ModelosColchonFacturaDAO.java
+++ src/dao/ModelosColchonFacturaDAO.java
@@ -2,53 +2,53 @@ package dao;
 
 import java.sql.*;
 import java.util.ArrayList;
-import model.ModelosColchonFactura;
+import model.MattressModelInvoice;
 
-public class ModelosColchonFacturaDAO {
+public class MattressModelInvoiceDAO {
 
-    private Connection conexion;
+    private Connection connection;
 
-    public ModelosColchonFacturaDAO() throws SQLException {
-        conexion = ConexionBD.conectarse();
+    public MattressModelInvoiceDAO() throws SQLException {
+        connection = ConexionBD.conectarse();
     }
 
-    public void agregarModeloColchonFactura(ModelosColchonFactura modeloColchonFactura) {
-        try (PreparedStatement pstmt = conexion.prepareStatement(
+    public void addMattressModelInvoice(MattressModelInvoice mattressModelInvoice) {
+        try (PreparedStatement pstmt = connection.prepareStatement(
                 "INSERT INTO modelos_colchon_factura (id_modelo_colchon, id_factura_venta) " +
                 "VALUES (?, ?)")) {
-            pstmt.setInt(1, modeloColchonFactura.getId_modelo_colchon());
-            pstmt.setInt(2, modeloColchonFactura.getId_factura_venta());
+            pstmt.setInt(1, mattressModelInvoice.getId_modelo_colchon());
+            pstmt.setInt(2, mattressModelInvoice.getId_factura_venta());
             pstmt.executeUpdate();
 
-            System.out.println("Modelo de colchon facturado agregado: " + modeloColchonFactura.getId_modelo_colchon() + " - " + modeloColchonFactura.getId_factura_venta());
+            System.out.println("Modelo de colchon facturado agregado: " + mattressModelInvoice.getId_modelo_colchon() + " - " + mattressModelInvoice.getId_factura_venta());
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public ArrayList<ModelosColchonFactura> obtenerModelosColchonFactura() throws SQLException {
-        ArrayList<ModelosColchonFactura> lista_modelos = new ArrayList<>();
-        try (Statement stmt = conexion.createStatement()) {
+    public ArrayList<MattressModelInvoice> getMattressModelInvoices() throws SQLException {
+        ArrayList<MattressModelInvoice> modelList = new ArrayList<>();
+        try (Statement stmt = connection.createStatement()) {
             ResultSet rs = stmt.executeQuery("SELECT * FROM modelos_colchon_factura");
             while (rs.next()) {
-                int id_modelo_colchon = rs.getInt("id_modelo_colchon");
-                int id_factura_venta = rs.getInt("id_factura_venta");
-                ModelosColchonFactura modelo = new ModelosColchonFactura(id_modelo_colchon, id_factura_venta);
-                lista_modelos.add(modelo);
+                int id_model = rs.getInt("id_modelo_colchon");
+                int id_invoice = rs.getInt("id_factura_venta");
+                MattressModelInvoice model = new MattressModelInvoice(id_model, id_invoice);
+                modelList.add(model);
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return lista_modelos;
+        return modelList;
     }
 
-    public boolean actualizarModeloColchonFactura(ModelosColchonFactura modeloActualizado) {
-        try (PreparedStatement pstmt = conexion.prepareStatement(
+    public boolean updateMattressModelInvoice(MattressModelInvoice updatedModel) {
+        try (PreparedStatement pstmt = connection.prepareStatement(
                 "UPDATE modelos_colchon_factura SET id_factura_venta = ? " +
                 "WHERE id_modelo_colchon = ? AND id_factura_venta = ?")) {
-            pstmt.setInt(1, modeloActualizado.getId_factura_venta());
-            pstmt.setInt(2, modeloActualizado.getId_modelo_colchon());
-            pstmt.setInt(3, modeloActualizado.getId_factura_venta());
+            pstmt.setInt(1, updatedModel.getId_factura_venta());
+            pstmt.setInt(2, updatedModel.getId_modelo_colchon());
+            pstmt.setInt(3, updatedModel.getId_factura_venta());
             pstmt.executeUpdate();
 
             System.out.println("Modelo de colchon facturado actualizado");
@@ -59,14 +59,14 @@ public class ModelosColchonFacturaDAO {
         }
     }
 
-    public boolean eliminarModeloColchonFactura(int id_modelo_colchon, int id_factura_venta) {
-        try (PreparedStatement pstmt = conexion.prepareStatement(
+    public boolean deleteMattressModelInvoice(int id_model, int id_invoice) {
+        try (PreparedStatement pstmt = connection.prepareStatement(
                 "DELETE FROM modelos_colchon_factura WHERE id_modelo_colchon = ? AND id_factura_venta = ?")) {
-            pstmt.setInt(1, id_modelo_colchon);
-            pstmt.setInt(2, id_factura_venta);
+            pstmt.setInt(1, id_model);
+            pstmt.setInt(2, id_invoice);
             pstmt.executeUpdate();
 
-            System.out.println("Modelo de colchon facturado eliminado con ID: " + id_modelo_colchon + " - " + id_factura_venta);
+            System.out.println("Modelo de colchon facturado eliminado con ID: " + id_model + " - " + id_invoice);
             return true;
         } catch (SQLException e) {
             e.printStackTrace();
