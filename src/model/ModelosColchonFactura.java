0109eaeb68c03fe4b4fc16fb6aac5a42b816700c
XxSuarezxX <141874132+XxSuarezxX@users.noreply.github.com>
2024-05-29 23:45:51 -0500

    Update ModelosColchonFactura.java

--- src/model/ModelosColchonFactura.java
+++ src/model/ModelosColchonFactura.java
@@ -1,19 +1,19 @@
 package model;
 
-public class ModelosColchonFactura {
-    private int id_modelo_colchon;
-    private int id_factura_venta;
+public class MattressModelInvoice {
+    private int mattress_model_id;
+    private int sale_invoice_id;
 
-    public ModelosColchonFactura(int id_modelo_colchon, int id_factura_venta) {
-        this.id_modelo_colchon = id_modelo_colchon;
-        this.id_factura_venta = id_factura_venta;
+    public MattressModelInvoice(int mattress_model_id, int sale_invoice_id) {
+        this.mattress_model_id = mattress_model_id;
+        this.sale_invoice_id = sale_invoice_id;
     }
 
-    public int getId_modelo_colchon() {
-        return id_modelo_colchon;
+    public int getMattress_model_id() {
+        return mattress_model_id;
     }
 
-    public int getId_factura_venta() {
-        return id_factura_venta;
+    public int getSale_invoice_id() {
+        return sale_invoice_id;
     }
 }
